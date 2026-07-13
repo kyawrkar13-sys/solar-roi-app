fae54c2cde83d067ce95468d87a1deafce316b84
kyawrkar13-sys <kyawrkar13@gmail.com>
2026-07-13 22:57:21 +0630

    Update app.py

--- app.py
+++ app.py
@@ -419,11 +419,6 @@ if name == "main":
 
         host="0.0.0.0",
 
-        port=int(
-            os.environ.get(
-                "PORT",
-                5000
-            )
-        )
+        port=5000))
 
     )
