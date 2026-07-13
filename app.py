e21202c66ad13bf6a62cdf0cdfc6c221118c603d
kyawrkar13-sys <kyawrkar13@gmail.com>
2026-07-14 00:10:43 +0630

    Update app.py

--- app.py
+++ app.py
@@ -77,5 +77,10 @@ def home():
 
     return render_template_string(HTML_TEMPLATE)
 
+import os
+
 if name == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
